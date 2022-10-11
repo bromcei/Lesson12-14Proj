a430b4f278649a55027721ad6ec07a8843264b1e
tomas.ceida <tomceid@gmail.com>
2022-10-11 20:54:42 +0300

    minor fixes

--- Lesson12-14Proj/Program.cs
+++ Lesson12-14Proj/Program.cs
@@ -1,16 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-
-using Lesson12_14Proj.Repositories;
+﻿using Lesson12_14Proj.Repositories;
 using Lesson12_14Proj.Service;
 using Lesson12_14Proj.Simuliation;
 
+WorkerRepository WorkersRep = new WorkerRepository();
+WorkersInWorkPlaceRepository WorkersInWorkPlaceRep = new WorkersInWorkPlaceRepository();
+GateRepository GateRep = new GateRepository();
+EventRepository EventsRep = new EventRepository();
 
-GateChecker gateCheck = new GateChecker(new WorkerRepository(), new GateRepository(), new WorkersInWorkPlaceRepository());
+GateChecker gateCheck = new GateChecker(WorkersRep, GateRep, WorkersInWorkPlaceRep);
 SimuliationService sim = new SimuliationService(gateCheck, "2022-10-06", "2023-10-05");
 sim.ClearEventsData();
 sim.SimulationStart();
 
 
-ReportGenerator reportGen = new ReportGenerator(new WorkerRepository(), new EventRepository());
+ReportGenerator reportGen = new ReportGenerator(WorkersRep, EventsRep);
 reportGen.DeleteAllReports();
 reportGen.GenerateAllReports();
